15ae3aa7c95e575a9287e132375ce5ee464bfa67
sudh-202 <87563365+sudh-202@users.noreply.github.com>
2023-01-28 16:47:05 +0530

    new changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -26,12 +26,15 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://dalle-arbb.onrender.com/api/v1/post', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
+      const response = await fetch(
+        "https://ai-image-3ng8.onrender.com/api/v1/post",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
       if (response.ok) {
         const result = await response.json();
